fa6d05116117f141c955e2783b9a794970a4ec27
Ayukito <alexbrowses@gmail.com>
2021-05-29 13:37:36 -0400

    Organization, folder for resources & scripts

---
+++ Scripts/convertBW2Icons.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd ..
+./rip 9;
+python3 Scripts/BWIconPalette.py 2;
+cd pokeIcons
+#bw has 711, bw2 has 750
+for i in {0..750}; do
+echo "converting $i to the right format!"
+convert -size 32x64 xc:black $i.bmp
+convert -extract 32x8+0+0 $i.png tmp.bmp
+composite -compose atop -geometry +0+0 tmp.bmp $i.bmp $i.bmp
+convert -extract 32x8+32+0 $i.png tmp.bmp
+composite -compose atop -geometry +0+8 tmp.bmp $i.bmp $i.bmp
+convert -extract 32x8+0+8 $i.png tmp.bmp
+composite -compose atop -geometry +0+16 tmp.bmp $i.bmp $i.bmp
+convert -extract 32x8+32+8 $i.png tmp.bmp
+composite -compose atop -geometry +0+24 tmp.bmp $i.bmp $i.bmp
+convert -extract 32x8+0+16 $i.png tmp.bmp
+composite -compose atop -geometry +0+32 tmp.bmp $i.bmp $i.bmp
+convert -extract 32x8+32+16 $i.png tmp.bmp
+composite -compose atop -geometry +0+40 tmp.bmp $i.bmp $i.bmp
+convert -extract 32x8+0+24 $i.png tmp.bmp
+composite -compose atop -geometry +0+48 tmp.bmp $i.bmp $i.bmp
+convert -extract 32x8+32+24 $i.png tmp.bmp
+composite -compose atop -geometry +0+56 tmp.bmp $i.bmp $i.bmp
+
+convert -transparent black $i.bmp $i.bmp
+
+convert $i.bmp $i.png
+
+rm -f tmp.bmp
+rm -f $i.bmp
+done;
---
+++ Scripts/rip-sprite.scm
@@ -0,0 +1,13 @@
+(define filename "Resources/pokegra-hg.narc")
+(define outfile "scmtest.png")
+
+(let* ((narc (load-narc filename)))
+  (let* ((ncgr (narc-load-file narc 8 'NCGR))
+         (nclr (narc-load-file narc 10 'NCLR))
+         (image (make-image)))
+
+    (ncgr-decrypt-pt ncgr)
+    (image-set-pixels-from-ncgr image ncgr)
+    (image-set-palette-from-nclr image nclr)
+
+    (image-save-png image outfile)))
